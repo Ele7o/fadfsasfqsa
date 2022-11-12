3bcd4f2c9e68a85e7c289ec4d1fdc40b01da840e
Ele7o <52443049+Ele7o@users.noreply.github.com>
2022-11-12 15:49:43 +0700

    Init Project

---
+++ DeQui.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_BuiThanhSon_1751010125
+{
+    public class DeQui
+    {
+        public static double Power(double x, int n)
+        {
+            if (n == 0)
+                return 1.0;
+            else if (n > 0)
+                return n * Power(x, n - 1);
+            else
+                return Power(x, n + 1) / x;
+        }
+    }
+
+}
+                       
---
+++ Poly.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_BuiThanhSon_1751010125
+{
+    public class Poly
+    {
+
+            private int n;
+            private List<int> a;
+            public Poly(int n, List<int> a)
+            {
+                if (a.Count() != n + 1)
+                    throw new ArgumentException("Invalid Data");
+                this.n = n;
+                this.a = a;
+            }
+            public int Cal(double x)
+            {
+                int result = 0;
+                for (int i = 0; i <= this.n; i++)
+                {
+                    result += (int)(a[i] * Math.Pow(x, i));
+                }
+                return result;
+            }
+        
+    }
+}
